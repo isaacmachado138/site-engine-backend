122bac5226e6ee427916e978348dcbe80067704f
isaac.machado <isaac.machado@35.168.70.227>
2025-06-05 15:22:20 -0300

    feat: new fields

--- application/interfaces/repositories/ISiteRepository.go
+++ application/interfaces/repositories/ISiteRepository.go
@@ -7,8 +7,8 @@ type SiteFilters struct {
 	UserID     *string `json:"user_id,omitempty"`
 	CategoryID *uint   `json:"category_id,omitempty"`
 	Active     *bool   `json:"active,omitempty"`
-	Name       *string `json:"name,omitempty"`       // Filtro por nome do site (com LIKE)
-	CityID     *uint   `json:"city_id,omitempty"`    // Filtro por cidade
+	Name       *string `json:"name,omitempty"`    // Filtro por nome do site (com LIKE)
+	CityID     *uint   `json:"city_id,omitempty"` // Filtro por cidade
 }
 
 // SiteRepository define os métodos para o repositório de sites
